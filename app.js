f58d352d58b811c0b4992c4648c777f8669bee05
Yahya <home@Yahyas-Macbook.local>
2024-10-04 12:08:22 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+// // Array to store to-do items
+// let toDoList = [];
+
+// // Function to add a new item to the to-do list
+// function addItem(item) {
+//     toDoList.push(item);
+//     console.log(`Added: ${item}`);
+// }
+
+// // Function to remove an item from the to-do list
+// function removeItem(index) {
+//     if (index >= 0 && index < toDoList.length) {
+//         const removedItem = toDoList.splice(index, 1);
+//         console.log(`Removed: ${removedItem}`);
+//     } else {
+//         console.log('Invalid index');
+//     }
+// }
+
+// // Function to display all items in the to-do list
+// function displayList() {
+//     console.log('To-Do List:');
+//     toDoList.forEach((item, index) => {
+//         console.log(`${index + 1}. ${item}`);
+//     });
+// }
+
+// // Example usage
+// addItem('Buy groceries');
+// addItem('Walk the dog');
+// displayList();
+// removeItem(0);
+// displayList();
